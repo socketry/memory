6c854ee2a4ed9af9957d9af7964da0d482b43b03
Michael Go <michael.go@shopify.com>
2025-11-12 19:03:15 -0400

    update unit test to be more simpler

--- test/memory/sampler.rb
+++ test/memory/sampler.rb
@@ -4,8 +4,6 @@
 # Copyright, 2020-2025, by Samuel Williams.
 
 require "memory"
-require "socket"
-require "fileutils"
 
 class MyThing
 end
@@ -42,50 +40,23 @@ describe Memory::Sampler do
 	end
 	
 	it "safely captures locked string objects" do
-		socket_path = "/tmp/test_supervisor.ipc"
-		FileUtils.rm_f(socket_path)
+		fiber = Fiber.new do |string|
+			IO::Buffer.for(string) do
+				# string is now locked for the duration of this block.
+				sleep(0.1)
+				Fiber.yield
+			end
+		end
 		
 		memory = Memory::Sampler.new
 		memory.start
 		
-		# Create server thread
-		Thread.new do
-			server = UNIXServer.new(socket_path)
-			client = server.accept
-			
-			2.times do
-				buffer = String.new(capacity: 2)
-				length_data = client.read(2, buffer) # buffer gets locked while reading
-				break unless length_data && length_data.bytesize == 2
-				
-				length = length_data.unpack1("n")
-				client.read(length)
-			end
-		ensure
-			client&.close
-			server.close
-		end
-		
-		# Create a client thread
-		Thread.new do
-			socket = UNIXSocket.new(socket_path)
-			
-			2.times do
-				message = Time.now.to_s
-				socket.write([message.bytesize].pack("n") + message)
-				puts "hello #{message}"
-				sleep(1)
-			end
-		ensure
-			socket&.close
-		end
-		
-		sleep(0.1)
+		# Lock the string
+		key = String.new("foo")
+		fiber.resume(key)
 		
 		memory.stop
 		memory.report # buffer string is locked while reading ObjectSpace#each_object
-	ensure
-		FileUtils.rm_f(socket_path)
 	end
 	
 	with "#as_json" do
