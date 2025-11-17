7efd329b420904b4acdaecb10cae5ed4d04e6a4b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-11-17 17:54:25 +1300

    Remove unnecessary `String.new << ...`.

--- test/memory/sampler.rb
+++ test/memory/sampler.rb
@@ -59,6 +59,48 @@ describe Memory::Sampler do
 		memory.report # buffer string is locked while reading ObjectSpace#each_object
 	end
 	
+	it "does not retain string references" do
+		# Ruby 3.2 has different shared string behaviour:
+		skip_unless_minimum_ruby_version("3.3")
+		
+		# Strings longer than 23 characters share a reference to a "shared" frozen string which should also be GC'd
+		sampler.run do
+			5.times do |i|
+				short_text = "SHORT TEXT ##{i}"
+				short_text.dup
+				
+				long_text = "LONG TEXT ##{i} 12345678901234567890123456789012345678901234567890"
+				long_text.dup
+				
+				very_long_text = "VERY LONG TEXT ##{i} 12345678901234567890123456789012345678901234567890 12345678901234567890123456789012345678901234567890 12345678901234567890123456789012345678901234567890 12345678901234567890123456789012345678901234567890 12345678901234567890123456789012345678901234567890"
+				very_long_text.dup
+				
+				# Prevent the last frozen string from being the return value of the block:
+				nil
+			end
+		end
+		
+		# 30 strings should be allocated (5 iterations * 6 strings per iteration):
+		# - short_text (interpolated result)
+		# - short_text.dup
+		# - long_text (interpolated result)
+		# - long_text.dup
+		# - very_long_text (interpolated result)
+		# - very_long_text.dup
+		expect(sampler.allocated.size).to be == 30
+		
+		# Get unique string values:
+		string_allocations = sampler.allocated.select{|a| a.class_name == "String"}
+		unique_strings = string_allocations.group_by(&:value).size
+		
+		# 15 unique strings (5 iterations * 3 unique strings per iteration):
+		expect(unique_strings).to be == 15
+		
+		# No strings should be retained (all were eligible for GC):
+		retained_strings = string_allocations.select(&:retained)
+		expect(retained_strings.size).to be == 0
+	end
+	
 	with "#as_json" do
 		it "returns allocation count" do
 			x = nil
